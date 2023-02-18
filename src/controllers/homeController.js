1e22be7a17fe75167cdd40e4c07494932dec7d86
Ngo Bao Lam <ngobaolam31122002@gmail.com>
2023-02-19 00:35:47 +0700

    Display data from database to Web screen

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -21,8 +21,19 @@ let postCRUD = async (req, res) => {
       console.log(message);
       return res.send('post crud from server');
 }
+
+let displayGetCRUD = async (req, res) => {
+      let data = await CRUDservice.getAllUser();
+      console.log('-------------')
+      console.log(data)
+      console.log('-------------')
+      return res.render('display-crud.ejs', {
+            dataTable: data
+      });
+}
 module.exports = {
       getHomePage: getHomePage,
       getCRUD: getCRUD,
       postCRUD: postCRUD,
+      displayGetCRUD: displayGetCRUD,
 }
